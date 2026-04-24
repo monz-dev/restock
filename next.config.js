a116b6a85e18bba70c6ecfa549f088c472f42a64
monz-dev <monzdev707@gmail.com>
2026-04-24 17:37:31 -0600

    fix: next config and manifiest

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: false,
   images: {
     domains: ['api.qrserver.com'],
+  },
+  webpack: (config) => {
+    config.optimization.splitChunks.cacheGroups = {
+      ...config.optimization.splitChunks.cacheGroups,
+      default: false,
+      vendors: false,
+    };
+    return config;
   },
 };
 
